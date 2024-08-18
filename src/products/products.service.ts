f98b1c23caf43f5621c380c45cc6e88272000126
JaviSrz <ljsuarez7@gmail.com>
2024-08-18 11:17:21 +0200

    Implementación del CRUD

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit{
@@ -15,23 +16,86 @@ export class ProductsService extends PrismaClient implements OnModuleInit{
 
   create(createProductDto: CreateProductDto) {
 
-    
+    return this.product.create({
+      data: createProductDto
+    });
 
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto: PaginationDto) {
+
+    const {page, limit} = paginationDto;
+
+    const totalPages = await this.product.count({where: {available: true}}); //Esto en realidad deberia llamarse total porque es el total de productos
+
+    const lastPage = Math.ceil(totalPages / limit); //Esto seria el total de paginas
+
+    //Con toda esta info se podria mandar un error si se pide una pagina q no tiene productos
+
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * 10,
+        take: limit,
+        where: {
+          available: true
+        }
+      }),
+      meta: {
+        total: totalPages,
+        page: page,
+        lastPage: lastPage
+      }
+    }
+
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+
+    const product = await this.product.findFirst({
+      where: {id, available: true}
+    });
+
+    if(!product){
+      throw new NotFoundException(`Product with id #${id} not found`);
+    }
+
+    return product;
+
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    //Se puede comprobar si el updateProductDto viene vacio entonces q no haga nada, porque no hay nada q actualizar
+
+    await this.findOne(id);
+
+    return this.product.update({
+      where: {id},
+      data: updateProductDto
+    });
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+
+    await this.findOne(id);
+
+    //HARD DELETE
+
+    // return this.product.delete({
+    //   where: {id}
+    // });
+
+    //SOFT DELETE
+
+    const product = await this.product.update({
+      where: {id},
+      data: {
+        available: false
+      }
+    });
+
+    return product;
+
   }
 }
