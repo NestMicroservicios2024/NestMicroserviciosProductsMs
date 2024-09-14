4108e5f59c01b65832aace8e4f2f2ee85954315b
JaviSrz <ljsuarez7@gmail.com>
2024-09-14 11:17:58 +0200

    Cambio de TCP A NATS

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,17 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
-  const logger = new Logger('Main');
+  const logger = new Logger('Main');  
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.TCP,
-    options: {
-      port: envs.port
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      }
     }
-  });
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
